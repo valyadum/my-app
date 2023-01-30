c6ee570b12ddcb30e2aa73e6d0ae01707247d188
Valentyna Dumbrava <valyamoldovan55555@outlook.com>
2023-01-30 17:20:27 +0200

    додавання ав та стилізація

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,11 +1,11 @@
 let state = {
     dialogsPage:{
      dialogs:[
-        { id: 1, name: "Dimych" },
-        { id: 2, name: "Andrew" },
-        { id: 3, name: "Sasha" },
-        { id: 4, name: "Victor" },
-        { id: 5, name: "Valera" },
+        { id: 1, name: "Dimych", avatar:"https://3.bp.blogspot.com/-8RizvKvIG0w/UzfYbn3_MQI/AAAAAAAAA7A/pKR9oL_1s64/s1600/ngBVYr_NKzI.jpg"},
+        { id: 2, name: "Andrew", avatar:"https://www.volynpost.com/img/modules/news/a/93/e049aec18db34feafa7eff11598b193a/cb-101726918408622392735281898852471n.jpg" },
+        { id: 3, name: "Sasha", avatar:"https://www.volynpost.com/img/modules/news/e/1c/fced9d71d3a27fb3bf5751597e4451ce/cb-i138_6cx6g.jpg" },
+        { id: 4, name: "Victor", avatar:"https://www.volynpost.com/img/modules/news/e/1c/fced9d71d3a27fb3bf5751597e4451ce/cb-cv2px3xu5qo.jpg" },
+        { id: 5, name: "Valera", avatar:"https://www.volynpost.com/img/modules/news/a/2d/89c6db59bcc5faedda072e84c6e432da/cb-yz2dikefvsy.jpg" },
       ],
       messages:[
         { id: 1, message: "Hi" },
@@ -19,7 +19,7 @@ let state = {
       posts:[
         { id: 1, message: "Hi, how are you?", like: "43" },
         { id: 2, message: "It's my first post", like: "98" },
-      ]  
+      ]
     }
 }
 export default state;
